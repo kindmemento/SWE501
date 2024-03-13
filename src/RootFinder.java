269792c8c45605d4f15f83572f6ba921eb229dbc
Metin Dökmen <metindokmen@192.168.1.105>
2024-03-13 16:55:23 +0300

    add rootfinder class, add empty main method, implement computeFunction private method

---
+++ src/RootFinder.java
@@ -0,0 +1,11 @@
+public class RootFinder {
+    private static final double EPSILON = 1e-6; // Tolerance for the root approximation
+
+    public static void main(String[] args) {
+        // @TODO: Implement main function body
+    }
+
+    private static double computeFunction(double x) {
+        return 0.1 * Math.pow(x, 3) + Math.pow(x, 2) - 6;
+    }
+}
