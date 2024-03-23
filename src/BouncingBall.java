a8bc95cfd95728f431ce8a8f73cb19afa821b51b
kindmemento <kindmemento@gmail.com>
2024-03-23 18:00:23 +0300

    add ball class, declare private variables, make import for color type

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -1,5 +1,17 @@
+import java.awt.Color;
+
 public class BouncingBall {
     public static void main(String[] args) {
         System.out.println("This is a bouncing ball game.");
     }
+}
+
+class Ball {
+    private double xCenter;
+    private double yCenter;
+    private double velocityX;
+    private double velocityY;
+    private double ballRadius;
+    private Color ballColor;
+
 }
