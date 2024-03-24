9c7a2dfdb5f58a25eedbf5c1326295df3b006751
kindmemento <kindmemento@gmail.com>
2024-03-24 16:47:35 +0300

    do separate checks of collision for vertical and horizontal sides of the obstacle

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -4,7 +4,31 @@ import java.util.ArrayList;
 
 public class BouncingBall {
     public static void main(String[] args) {
-        System.out.println("This is a bouncing ball game.");
+        StdDraw.setCanvasSize(800, 800);
+        StdDraw.enableDoubleBuffering();
+
+        ArrayList<Obstacle> obstacles = new ArrayList<>();
+        obstacles.add(new Obstacle(0.1, 0.8, 0.2, 0.05));
+        obstacles.add(new Obstacle(0.7, 0.3, 0.1, 0.1));
+        obstacles.add(new Obstacle(0.4, 0.6, 0.15, 0.03));
+
+        Ball ball = new Ball();
+
+        while (true) {
+            StdDraw.clear();
+
+            // Draw obstacles
+            for (Obstacle obstacle : obstacles) {
+                obstacle.draw();
+            }
+
+            ball.move();
+            ball.checkCollision(obstacles);
+            ball.draw();
+
+            StdDraw.show();
+            StdDraw.pause(20);
+        }
     }
 }
 
@@ -44,11 +68,21 @@ class Ball {
 
     public void checkCollision(ArrayList<Obstacle> obstacles) {
         for (Obstacle obstacle : obstacles) {
+            // Check collision with vertical sides of the obstacle
             if (xCenter + ballRadius >= obstacle.getXCenter() - obstacle.getWidth() / 2 &&
                 xCenter - ballRadius <= obstacle.getXCenter() + obstacle.getWidth() / 2 &&
-                yCenter + ballRadius >= obstacle.getYCenter() - obstacle.getHeight() / 2 &&
-                yCenter - ballRadius <= obstacle.getYCenter() + obstacle.getHeight() / 2) {
+                yCenter >= obstacle.getYCenter() - obstacle.getHeight() / 2 &&
+                yCenter <= obstacle.getYCenter() + obstacle.getHeight() / 2) {
                 velocityX = -velocityX;
+                break;
+            }
+
+            // Check collision with horizontal sides of the obstacle
+            if (yCenter + ballRadius >= obstacle.getYCenter() - obstacle.getHeight() &&
+                yCenter - ballRadius <= obstacle.getYCenter() + obstacle.getHeight() &&
+                xCenter >= obstacle.getXCenter() - obstacle.getWidth() / 2 &&
+                xCenter <= obstacle.getXCenter() + obstacle.getWidth()
+            ) {
                 velocityY = -velocityY;
                 break;
             }
