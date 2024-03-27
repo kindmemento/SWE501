045d6078749a4d0f2cee322665ae749dd69ae934
kindmemento <kindmemento@gmail.com>
2024-03-27 10:58:34 +0300

    simplify and refactor

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -5,30 +5,34 @@ import java.util.ArrayList;
 public class BouncingBall {
     public static void main(String[] args) {
         StdDraw.setCanvasSize(800, 800);
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(0, 100);
         StdDraw.enableDoubleBuffering();
 
-        ArrayList<Obstacle> obstacles = new ArrayList<>();
-        obstacles.add(new Obstacle(0.1, 0.8, 0.2, 0.05));
-        obstacles.add(new Obstacle(0.7, 0.3, 0.1, 0.1));
-        obstacles.add(new Obstacle(0.4, 0.6, 0.15, 0.03));
+        Obstacle[] obstacles = {
+                new Obstacle(40, 20),
+                new Obstacle(60, 80)
+        };
 
-        Ball ball = new Ball();
+        Ball ball = new Ball(20, 50, 1, 1, 3, Color.RED);
 
         while (true) {
-            StdDraw.clear();
+            StdDraw.clear(Color.WHITE);
 
-            // Draw obstacles
             for (Obstacle obstacle : obstacles) {
                 obstacle.draw();
             }
 
             ball.move();
-            ball.checkCollision(obstacles);
             ball.draw();
 
+            ball.bounce(0, 100, 0, 100, obstacles);
+
             StdDraw.show();
+
             StdDraw.pause(20);
         }
+
     }
 }
 
@@ -40,14 +44,13 @@ class Ball {
     private double ballRadius;
     private Color ballColor;
 
-    public Ball() {
-        Random random = new Random();
-        xCenter = random.nextDouble();
-        yCenter = random.nextDouble();
-        velocityX = random.nextDouble() * 0.03 - 0.01;
-        velocityY = random.nextDouble() * 0.03 - 0.01;
-        ballRadius = 0.02;
-        ballColor = Color.BLUE;
+    public Ball(double xCenter, double yCenter, double velocityX, double velocityY, double ballRadius, Color ballColor) {
+        this.xCenter = xCenter;
+        this.yCenter = yCenter;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.ballRadius = ballRadius;
+        this.ballColor = ballColor;
     }
 
     public void draw() {
@@ -58,33 +61,30 @@ class Ball {
     public void move() {
         xCenter += velocityX;
         yCenter += velocityY;
-        if (xCenter + ballRadius >= 1 || xCenter - ballRadius <= 0) {
-            velocityX = -velocityX;
-        }
-        if (yCenter + ballRadius >= 1 || yCenter - ballRadius <= 0) {
-            velocityY = -velocityY;
-        }
     }
 
-    public void checkCollision(ArrayList<Obstacle> obstacles) {
-        for (Obstacle obstacle : obstacles) {
-            // Check collision with vertical sides of the obstacle
-            if (xCenter + ballRadius >= obstacle.getXCenter() - obstacle.getWidth() / 2 &&
-                xCenter - ballRadius <= obstacle.getXCenter() + obstacle.getWidth() / 2 &&
-                yCenter >= obstacle.getYCenter() - obstacle.getHeight() / 2 &&
-                yCenter <= obstacle.getYCenter() + obstacle.getHeight() / 2) {
-                velocityX = -velocityX;
-                break;
-            }
+    public void bounce(double xMin, double xMax, double yMin, double yMax, Obstacle[] obstacles) {
+        if (xCenter - ballRadius <= xMin || xCenter + ballRadius >= xMax) {
+            velocityX *= -1;
+        }
 
-            // Check collision with horizontal sides of the obstacle
-            if (yCenter + ballRadius >= obstacle.getYCenter() - obstacle.getHeight() &&
-                yCenter - ballRadius <= obstacle.getYCenter() + obstacle.getHeight() &&
-                xCenter >= obstacle.getXCenter() - obstacle.getWidth() / 2 &&
-                xCenter <= obstacle.getXCenter() + obstacle.getWidth()
-            ) {
-                velocityY = -velocityY;
-                break;
+        if (yCenter - ballRadius <= yMin || yCenter + ballRadius >= yMax) {
+            velocityY *= -1;
+        }
+
+        for (Obstacle obstacle : obstacles) {
+            double obstacleLeft = obstacle.getXCenter() - 0.5;
+            double obstacleRight = obstacle.getXCenter() + 0.5;
+            double obstacleTop = obstacle.getYCenter() + 0.5;
+            double obstacleBottom = obstacle.getYCenter() - 0.5;
+
+            if (xCenter + ballRadius >= obstacleLeft && xCenter - ballRadius <= obstacleRight &&
+                yCenter + ballRadius >= obstacleBottom && yCenter - ballRadius <= obstacleTop) {
+                if (Math.abs(xCenter - obstacle.getXCenter()) > Math.abs(yCenter - obstacle.getYCenter())) {
+                    velocityX *= -1;
+                } else {
+                    velocityY *= -1;
+                }
             }
         }
     }
@@ -93,19 +93,15 @@ class Ball {
 class Obstacle {
     private double xCenter;
     private double yCenter;
-    private double width;
-    private double height;
 
-    public Obstacle(double xCenter, double yCenter, double width, double height) {
+    public Obstacle(double xCenter, double yCenter) {
         this.xCenter = xCenter;
         this.yCenter = yCenter;
-        this.width = width;
-        this.height = height;
     }
 
     public void draw() {
         StdDraw.setPenColor(Color.BLACK);
-        StdDraw.filledRectangle(xCenter, yCenter, width / 2, height / 2);
+        StdDraw.rectangle(xCenter, yCenter, 7.5, 10.0);
     }
 
     public double getXCenter() {
@@ -114,13 +110,5 @@ class Obstacle {
 
     public double getYCenter() {
         return yCenter;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
     }
 }
